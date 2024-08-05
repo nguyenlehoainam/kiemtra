67024ba611a8bcc97a840bb0ffa619326ac49265
nguyenlehoainam <namngyenhoai21@gmail.com>
2024-08-05 19:50:41 +0700

    kiem tra

---
+++ bai 1.py
@@ -0,0 +1,14 @@
+
+my_tuple = tuple(map(int, input("nhap mot loat cac so nguyen : ").split()))
+
+my_dict = {}
+
+for num in my_tuple:
+    if num in my_dict:
+        my_dict[num] += 1
+    else:
+        my_dict[num] = 1
+
+
+print("cac so thoa man la:", [num for num, count in my_dict.items() if count % 5 == 0 and count % 10 != 0])
+print(type(my_dict))
---
+++ bai 2.py
@@ -0,0 +1,9 @@
+
+x = int(input("nhap dich den chu rua can phai di X (1<x<1000) : "))
+
+so_buoc = x // 3  
+if x % 3 != 0:
+    so_buoc += 1  
+
+
+print("so buoc toi thieu la :", so_buoc)
